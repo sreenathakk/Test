08e879206ecd797a0f0a0ece2598ab01fb146564
sreenathakk <sreenatha.kk@gmail.com>
2018-05-02 19:34:19 +0530

    Add files via upload

---
+++ DomainJoin.ps1
@@ -0,0 +1,17 @@
+﻿Param(
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $addomain,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [string] $DomainAdmin,
+
+    [Parameter(Mandatory=$True)]
+    [ValidateNotNullOrEmpty()]
+    [securestring] $DomainPassword
+)
+
+$Domainusername = "$addomain\$DomainAdmin" 
+$credential = New-Object System.Management.Automation.PSCredential($Domainusername,$DomainPassword)
+Add-Computer -DomainName $addomain -Credential $credential
